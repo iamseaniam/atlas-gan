9969edec07f9544b1cfa2c15f7d0972088da4891
iamseaniam <7222@holbertonstudents.com>
2024-10-24 16:19:32 -0500

    added diciminaotr loss and generator loss functions

--- Dcgan/Baseline.py
+++ Dcgan/Baseline.py
@@ -77,3 +77,22 @@ def make_discriminator_model():
 
     return model
 
+discriminator = make_discriminator_model()
+decision = discriminator(generated_image)
+print (decision)
+
+# HL: This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
