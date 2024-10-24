49125cd3c837f63803448e54ce9b5c83c054a845
iamseaniam <7222@holbertonstudents.com>
2024-10-24 15:12:28 -0500

    importing all nesscessary  import

--- Dcgan/main.py
+++ Dcgan/main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 """"""
+import tensorflow as tf
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+
+from IPython import display
+
+
