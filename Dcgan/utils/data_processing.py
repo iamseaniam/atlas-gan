3b93eedb3b9410c85d350cd2fe1b6775d161f508
iamseaniam <7222@holbertonstudents.com>
2024-10-23 20:08:05 -0500

    in data_process file added saving data and the ability to call function

--- Dcgan/utils/data_processing.py
+++ Dcgan/utils/data_processing.py
@@ -5,9 +5,17 @@ import numpy as np
 from tensorflow.keras.datasets import mnist
 
 
-def preprocess_savedata():
+def preprocess_save_data():
     """ Preprocessing data and saving it """
     (x_train, _), (x_test, _) = mnist.load_data()
     # hl: normalize to [0, 1] and expand dimensions to add a channel
     x_train = np.expand_dims(x_train, axis=-1) / 255.0
     x_test = np.expand_dims(x_test, axis=-1) / 255.0
+
+    # hl: Saves preprocessed data
+    np.savez_compressed('data/mnist_train.npz', x_train)
+    np.savez_compressed('data/mnist_test.npz', x_test)
+
+# ! Call this function to preprocess and save the data
+if __name__ == '__main__':
+    preprocess_save_data
