5c799d3e960acc4b2cd68d237d58c20f2d9ead21
iamseaniam <7222@holbertonstudents.com>
2024-10-24 16:59:53 -0500

    creating gif functions

--- Dcgan/Baseline.py
+++ Dcgan/Baseline.py
@@ -175,3 +175,22 @@ train(train_dataset, EPOCHS)
 
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
+# HL: Display a single image using the epoch number 
+def display_image(epoch_no):
+    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
+display_image(EPOCHS)
+
+anim_file = 'dcgan.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer
+    filenames = glob.glob('image*.png')
+    filenames = sorted(filenames)
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+    image = imageio.imread(filename)
+    writer.append_data(image)
+
+import tensorflow_docs.vis.embed as embed
+embed.embed_file(anim_file)
