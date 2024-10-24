97f562abdb3e274eef3fd8903e830cb87b914992
iamseaniam <7222@holbertonstudents.com>
2024-10-24 15:56:15 -0500

    fixing errors

--- Dcgan/Baseline.py
+++ Dcgan/Baseline.py
@@ -25,13 +25,15 @@ train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_
 
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    
+    model.add(layers.Dense(7*7*256, use_bias=False))
     model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU)
+    model.add(layers.LeakyReLU())
 
     model.add(layers.Reshape((7, 7, 256)))
     assert model.output_shape == (None, 7, 7, 256) # HL: Note: None is the batch size
 
+    # todo: try kernel_initializer='glorot_normal'))
     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
     assert model.output_shape == (None, 7, 7, 128)
     model.add(layers.BatchNormalization())
@@ -39,7 +41,7 @@ def make_generator_model():
 
     model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
     assert model.output_shape == (None, 14, 14, 64)
-    model.add(layers.BatchNormal())
+    model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
