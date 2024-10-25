200c801096dd9860593b26cecf88c2987755f87f
iamseaniam <7222@holbertonstudents.com>
2024-10-25 08:28:45 -0500

    files for first run for model

--- Baseline/Baseline.py
+++ Baseline/Baseline.py
@@ -134,6 +134,22 @@ def train_step(images):
 
     return gen_loss, disc_loss
 
+def generate_and_save_images(model, epoch, test_input):
+    # Notice `training` is set to False.
+    # This is so all layers run in inference mode (batchnorm).
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.close
+    #! plt.show() removed, no need to close plots either
+
 def train(dataset, epochs):
     for epoch in range(epochs):
         start = time.time()
@@ -171,24 +187,6 @@ def train(dataset, epochs):
     # Generate after the final epoch
     display.clear_output(wait=True)
     generate_and_save_images(generator, epochs, seed)
-
-train(train_dataset, EPOCHS)
-
-def generate_and_save_images(model, epoch, test_input):
-    # Notice `training` is set to False.
-    # This is so all layers run in inference mode (batchnorm).
-    predictions = model(test_input, training=False)
-
-    fig = plt.figure(figsize=(4, 4))
-
-    for i in range(predictions.shape[0]):
-        plt.subplot(4, 4, i+1)
-        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-        plt.axis('off')
-
-    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-    #! plt.show() removed, no need to close plots either
-
 
 train(train_dataset, EPOCHS)
 
